987399b972009cc9f32657757563bb1dbbea8c60
Yu-Ju <jessicayujuchen@gmail.com>
2023-05-19 08:49:43 +0200

    Added comments and assumptions

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,20 +6,23 @@ import java.util.Scanner;
 
 /**
  * @author Yu-Ju Chen
+ * Implementation whereby a string input produces a comma delimited list of numbers grouping the numbers into
+ * a range when they are sequential.
+ * Assumption: input values are taken from a textfile.
  */
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         NumberRangeSummarizer solution = new Solution();
         String inputData = readInputFile("src/main/input.txt");
-        String output = solution.summarizeCollection(solution.collect(inputData));
-        System.out.println("Result: " + output);
+        String obtainedOutput = solution.summarizeCollection(solution.collect(inputData));
+        System.out.println("Result: " + obtainedOutput);
     }
 
     /**
-     * Method used to read data from input.txt, and returns a valid string to be summarized into ranges
+     * Method used to read data from a textfile, and returns a valid string to be summarized into ranges
      */
-    public static String readInputFile(String fileName) {
+    public static String readInputFile(String fileName){
         String inputData = "";
         try {
             Scanner reader = new Scanner(new File(fileName));
